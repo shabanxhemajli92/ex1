55d80df0392aa45f944805762c48210171d07387
Shaban <shabanxhemajli@live.com>
2022-08-09 15:04:33 +0200

    55

---
+++ exercise .py
@@ -0,0 +1,12 @@
+# do exercise here
+# upload to github for portfolio
+# thank me later when your git graph is as green as the python money u gonna earn
+
+print("Shaban Xhemajli")
+
+print(12345,"is type of" ,type(12345))
+
+print(isinstance,451,int)
+
+
+
