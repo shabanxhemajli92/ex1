4ecaaee1711e92c591261aa4a65aff2f1260efa1
Shaban <shabanxhemajli@live.com>
2022-08-09 15:30:03 +0200

    22

--- exercise .py
+++ exercise .py
@@ -2,11 +2,20 @@
 # upload to github for portfolio
 # thank me later when your git graph is as green as the python money u gonna earn
 
-print("Shaban Xhemajli")
 
-print(12345,"is type of" ,type(12345))
+# Task 1
+print(445) # a
+print(4.5) # b
+print(4+1j) # c
+print("Shaban Xhemajli") # d
+print(bool(0)) #e
 
-print(isinstance,451,int)
+# Task 2
+print(665,"is type of",type(665)) #a
+print(45.2,"is type of",type(45.2)) #b
+print(4-1j,"is type of",type(4-1j)) #c
+print("how are you","is type of",type("how are you"))
+print(True,"is type of",type(True))
 
-
-
+# Task3
+print(isinstance,123,int)
